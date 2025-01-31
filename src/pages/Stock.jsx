bc31cb25e030a257555134dc7b055a4c8a995cf0
vishal kumar shaw <vishalkumar.msit@gmail.com>
2025-01-31 18:47:17 +0530

    stock menu added

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -7,6 +7,7 @@ import Row from "../ui/Row";
 import RefillStockForm from "../features/stock/RefillStockForm";
 import StockTable from "../features/stock/StockTable";
 import CreateNewStockItem from "../features/stock/CreateNewStockItem";
+import { TiThMenu } from "react-icons/ti";
 
 const Stacked = styled.div`
   display: flex;
@@ -20,24 +21,31 @@ function Stocks() {
     <Menus>
       <Row type="horizontal">
         <Heading as="h4">Stock</Heading>
-        <Stacked>
-          <Modal>
-            <Modal.Open opens="addStockItem">
-              <Button size="small">Add Stock</Button>
-            </Modal.Open>
-
-            <Modal.Open opens="refillStock">
-              <Button size="small">Refill Stock</Button>
-            </Modal.Open>
-
+        <Modal>
+          <Menus.Menu>
+            <Menus.Toggle id={"stock"} icon={<TiThMenu />} />
+            <Menus.List id={"stock"}>
+              <Modal.Open opens="addStockItem">
+                <Menus.Button>Add Stock</Menus.Button>
+              </Modal.Open>
+              <Modal.Open opens="refillStock">
+                <Menus.Button>Refill Stock</Menus.Button>
+              </Modal.Open>
+              <Modal.Open opens="stockHistory">
+                <Menus.Button>Stock History</Menus.Button>
+              </Modal.Open>
+              <Modal.Open opens="suppliers">
+                <Menus.Button>Suppliers</Menus.Button>
+              </Modal.Open>
+            </Menus.List>
             <Modal.Window name="refillStock">
               <RefillStockForm />
             </Modal.Window>
             <Modal.Window name="addStockItem">
               <CreateNewStockItem />
             </Modal.Window>
-          </Modal>
-        </Stacked>
+          </Menus.Menu>
+        </Modal>
       </Row>
 
       <Row>
